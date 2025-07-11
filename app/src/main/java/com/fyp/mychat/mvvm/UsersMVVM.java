27408729a797fe0c015df9db9faa9f37effd59c3
Fiaz Zafar <fiazzafar430@gmail.com>
2025-07-11 01:45:22 -0700

    version 1.1

--- app/src/main/java/com/fyp/mychat/mvvm/UsersMVVM.java
+++ app/src/main/java/com/fyp/mychat/mvvm/UsersMVVM.java
@@ -1,20 +1,15 @@
 package com.fyp.mychat.mvvm;
 
-import android.util.Log;
-import android.widget.Toast;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.fyp.mychat.FirebaseHelpers.FriendDB;
 import com.fyp.mychat.FirebaseHelpers.UsersFB;
-import com.fyp.mychat.adapter.UserListAdapter;
 import com.fyp.mychat.helpers.StatusEnum;
 import com.fyp.mychat.interfaces.FirebaseCallbacks;
 import com.fyp.mychat.interfaces.FriendsInterface;
 import com.fyp.mychat.interfaces.UserInterFace;
-import com.fyp.mychat.model.FriendListModel;
 import com.fyp.mychat.model.UserModel;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -45,28 +40,26 @@ public class UsersMVVM extends ViewModel {
         Set<String> uniqueUsers = new HashSet<>();
         AtomicInteger counter  = new AtomicInteger();
 
-        userInterFace.fetchUsersList(result -> {
-            fetchFriendsList(userId, onList -> {
-                list.clear();
-                for (UserModel userModel : result) {
-                    synchronized (uniqueUsers){
-                        if (uniqueUsers.contains(userModel.getuId())){
-                            if (counter.decrementAndGet() == 0){
-                                userList.setValue(new ArrayList<>(list));
-                            }
-                            return;
+        userInterFace.fetchUsersList(result -> fetchFriendsList(userId, onList -> {
+            list.clear();
+            for (UserModel userModel : result) {
+                synchronized (uniqueUsers){
+                    if (uniqueUsers.contains(userModel.getuId())){
+                        if (counter.decrementAndGet() == 0){
+                            userList.setValue(new ArrayList<>(list));
                         }
-                        uniqueUsers.add(userModel.getuId());
+                        return;
                     }
-
-                    if (onList.contains(userModel.getuId())) {
-                        userModel.setFriendShipStatus(StatusEnum.Accepted);
-                    }
-                    list.add(userModel);
+                    uniqueUsers.add(userModel.getuId());
                 }
-                userList.setValue(list);
-            });
-        });
+
+                if (onList.contains(userModel.getuId())) {
+                    userModel.setFriendShipStatus(StatusEnum.Accepted);
+                }
+                list.add(userModel);
+            }
+            userList.setValue(list);
+        }));
     }
 
 }
